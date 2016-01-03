ec5f9d2944461be0c5979e21520007266a50024c
David ödling <davidodling@me.com>
2016-01-03 17:47:24 +0100

    Began work with sparse Gp and made some adjustments to the basic gp in the process. Please note that the sparse gp is not yet working!

--- gp.py
+++ gp.py
@@ -1,96 +1,175 @@
 from __future__ import division
 from theano import tensor as T
 from theano import function
+from theano import gradient
+from theano.tensor import slinalg
 import numpy as np
 from scipy.optimize import minimize
 from kernels import *
 from theano import shared
-
+"""
+Standard gaussian process implementation with ARD kernel. Hyperparams: sigma, c, b_1...,b_D.
+"""
 class GaussianProcess(object):
-    def __init__(self, xtrain, ytrain,kernel=None):
+    def __init__(self, xtrain, ytrain):
         self.xtrain = xtrain
         self.ytrain = ytrain
-        self.nuv = shared(np.ones((xtrain.shape[1])).astype(np.float32))
-        if(kernel == None):
-            self.kernel = ARDKernel()
-        else:
-            self.kernel = kernel
-        self.nu = self.kernel.nu
-        self.theta = self.kernel.theta
         self.N = self.xtrain.shape[0]
-        self.theta = 1.0
-        self.inverter = T.nlinalg.MatrixInverse()
+        self.D = self.xtrain.shape[1]
+        self.sigma = shared(np.float64(1.),"sigma")
+        self.kernel = ARDKernel(self.D)
+        self.b = self.kernel.b
+        self.c = self.kernel.c
         self.det = T.nlinalg.Det()
+        self.inverter = T.nlinalg.MatrixInverse()
+        self.params = [self.b,self.c,self.sigma]
+
+    def updateParams(updates):
+    	for param, update in zip(self.params,updates):
+    		param.set_value(update)
 
     def eval_kernel(self):
-    	return self.kernel.evaluate(self.xtrain, self.xtrain,self.nuv.eval(),self.theta)
+    	return self.kernel.evaluate(self.xtrain, self.xtrain)
         
-    def getPosteriorPredictive(self,pred_x = np.matrix(np.linspace(-2,2,100,dtype=np.float32)).T):
-        K_train=self.kernel.evaluate(self.xtrain,self.xtrain,self.nuv.eval(),self.theta) + T.pow(self.theta,2)*T.eye(self.xtrain.shape[0]); 
-        K_mix=self.kernel.evaluate(self.xtrain,pred_x,self.nuv.eval(),self.theta); 
-        K_predict=self.kernel.evaluate(pred_x,pred_x,self.nuv.eval(),self.theta);
+    def getPosteriorPredictive(self,pred_x):
+        K_train=self.kernel.evaluate(self.xtrain,self.xtrain) + T.pow(self.sigma,2)*T.eye(self.xtrain.shape[0]); 
+        K_mix=self.kernel.evaluate(self.xtrain,pred_x); 
+        K_predict=self.kernel.evaluate(pred_x,pred_x);
         K_k_inv = self.inverter(K_train);
         K_mixinv= T.dot(K_mix.T, K_k_inv);
         mu=T.dot(K_mixinv,self.ytrain);
         sigma_p=K_predict-T.dot(K_mixinv,K_mix);
-        return mu, sigma_p
-    
-    def sample(self,samples=10, domain = np.matrix(np.linspace(-2,2,100,dtype=np.float32)).T):
+        return mu.eval(), sigma_p.eval()
+
+    def sample(self,domain,samples=10):
         mu, cp = self.getPosteriorPredictive(domain)
-        return np.random.multivariate_normal(mu.T.eval()[0],cp.eval(),samples)
-    
+        return np.random.multivariate_normal(mu.T[0],cp,samples)
+
     def log_likelihood(self):
-        c = self.kernel.exp + T.pow(self.kernel.theta,2) * T.eye(self.kernel.exp.shape[0])
+        c = self.kernel.exp + T.pow(self.sigma,2.0) * T.eye(self.N)
+        cinv = self.inverter(c)
         t1 = 0.5*T.log(self.det(c))
-        t2 = 0.5*T.dot(T.dot(self.ytrain.T,self.inverter(c)),self.ytrain)
+        t2 = 0.5*T.dot(T.dot(self.ytrain.T,cinv),self.ytrain)
         t3 = 0.5*self.N*T.log(2.0*np.pi);
         tot = t1+t2+t3
-        return tot[0][0]
+        return tot.flatten()[0]
+
+
+	def np_log_likelihood(self):
+		c = self.kernel.evaluate(self.xtrain,self.xtrain) + np.power(sigma,2)*np.eye(self.xtrain.shape[0])
+		t1 = 0.5*np.log(np.linalg.det(c))
+		t2 = 0.5*np.dot(np.dot(self.ytrain.T,self.np.linalg.inv(c)),self.ytrain)
+		t3 = 0.5*self.N*np.log(2.0*np.pi)
+		return t1+t2+t3
 
     def getL(self):
-        return function(inputs=[self.kernel.X, self.kernel.Y,self.kernel.nu,self.kernel.theta],outputs=[self.log_likelihood()])
+        return function(inputs=[self.kernel.X, self.kernel.Xp],outputs=[self.log_likelihood()])
 
     def grad(self):
-        grad = T.grad(self.log_likelihood(),self.kernel.nu)
-        fn = function(inputs=[self.kernel.X,self.kernel.Y,self.nu,self.kernel.theta], outputs=[grad])
+        grad = T.grad(self.log_likelihood(),self.params) #+T.log(self.c)+T.sum(T.log(self.b))+T.log(self.sigma)
+        fn = function(inputs=[self.kernel.X,self.kernel.Xp], outputs=grad)
         return fn
 
-    def plain_gradient_descent(self,maxiters =1000, step=0.01, tresh = 0.001, stop_early = True):
-        params= self.nuv.eval()
+    def plain_gradient_descent(self,maxiters =1000, step=0.01):
         gf = self.grad()
         L = self.getL()
         costs = []
-        param = []
         iters = 0
         prevcost = np.infty
-        currcost = L(self.xtrain,self.xtrain,params,self.theta)[0]
+        currcost = L(self.xtrain,self.xtrain)[0]
         costs.append(currcost)
-        while iters<maxiters or (abs(prevcost-currcost)<tresh and stop_early):
-            g = gf(self.xtrain,self.xtrain,params,self.theta)[0]
-            costs.append(L(self.xtrain,self.xtrain,params,self.theta)[0])
-            #print params
-            param.append(params)
-            params -= step * g
+        while iters<maxiters:
+            updates = gf(self.xtrain,self.xtrain)
+            costs.append(L(self.xtrain,self.xtrain)[0])
+            for update,param in zip(updates,self.params):
+            	param.set_value(np.float64(param.get_value()-step * np.clip(update,-0.1,0.1)))
             iters+=1
             prevcost = costs[iters-1]
             currcost = costs[iters]
-        self.nuv.set_value(params)
-        return costs, params
-
-    def train_numpy_cost_grads(self, variables = []):
-        cost = self.log_likelihood()
-        grad = T.grad(cost,self.kernel.nu)
-        fn = function(inputs=[self.kernel.X,self.kernel.Y,self.nu,self.kernel.theta], outputs=[cost, grad], allow_input_downcast=True)
-        def train_fn(nu):
-            c, g = fn(self.xtrain,self.xtrain,np.float32(nu),np.float32(self.theta))
-            c = np.float64(c)
-            g = np.float64(g)
-            return c,g
-        x = self.nuv.eval()
+        return costs
+	"""	
+	Wrapper around scipy.minimize, vectorizes all hyperparams for compatibility.
+	"""
+	def train_numpy_cost_grads(self, method = " L-BFGS-B"):
+		cost = self.getL()
+        g = self.grad()
+        def train_fn(x):
+        	b = x[:self.D]
+        	c = x[self.D]
+        	sigma = x[self.D+1]
+        	self.b.set_value(np.float64(b),borrow=True)
+        	self.c.set_value(np.float64(c),borrow=True)
+        	self.sigma.set_value(np.float64(sigma),borrow=True)
+        	c = cost(self.xtrain,self.xtrain)
+        	grads = g(self.xtrain,self.xtrain)
+        	gr = list(grads[0])
+        	gr.append(np.array(grads[1]))
+        	gr.append(np.array(grads[2]))
+        	c = np.float64(c)
+        	gr = np.float64(gr)
+        	return c,gr
+        x = np.random.random(self.D+2)
         weights = minimize(train_fn,x,
-                    method='L-BFGS-B', jac=True,
+                    method=method, jac=True,bounds=tuple((0.,None) for x in range(self.D+2)),
                     options={'maxiter': 200, 'disp': True})
-        nw = weights.x
-        nw = np.asarray(nw,dtype=np.float32)
-        self.nuv.set_value(nw)
-        return weights
+        x = weights.x
+    	b = x[:self.D]
+    	c = x[self.D]
+    	sigma = x[self.D+1]
+    	self.b.set_value(np.float64(b),borrow=True)
+    	self.c.set_value(np.float64(c),borrow=True)
+    	self.sigma.set_value(np.float64(sigma),borrow=True)
+       	return weights
+
+"""
+Sparse Gaussian Processes using Pseudo-inputs, Snelson et al. 2006
+HYperparams: sigma, c, b_1...,b_D , X_11, ... X_1D ... X_MD
+"""
+class SparseGaussianProcess(GaussianProcess):
+	def __init__(self,xtrain,ytrain,M):
+		GaussianProcess.__init__(self,xtrain,ytrain)
+		self.M = M
+		self.pseudo_points = shared(self.xtrain[:self.M,:],"pseudo_inputs")
+		self.Kmm = ARDKernel(self.D)
+		self.Kmn = ARDKernel(self.D)
+		self.Knm = ARDKernel(self.D)
+		self.params = [self.b,self.c,self.sigma, self.pseudo_points]
+
+	def log_likelihood(self):
+	 	gamma = T.diag(self.kernel.exp) + T.power(self.sigma,2.)*T.eye(self.N)
+		gammainv = self.inverter(gamma)
+	 	Kmm = self.Kmm.exp
+	 	Knm = self.Knm.exp
+	 	Kmn = self.Kmn.exp
+	 	A = T.power(self.sigma,2)*Kmm + T.dot(T.dot(Knm,gammainv),Kmn);
+	 	Ainv = self.inverter(A)
+	 	psi1 = T.log(self.det(A)) + T.log(self.det(gamma))-T.log(self.det(Kmm))+(self.N-self.M)*T.log(T.power(self.sigma,2))
+	 	psi2 = T.dot(T.dot(T.power(1./self.sigma,2)*self.ytrain.T,gammainv-T.dot(T.dot(T.dot(T.dot(gammainv,Knm),Ainv),Kmn),gammainv)),self.ytrain)
+	 	return psi1+psi2
+
+	def getL(self):
+		return function(inputs=[self.kernel.X, self.kernel.Xp,self.Kmm.X,self.Kmm.Xp,self.Kmn.X,self.Kmn.Xp,self.Knm.X,self.Knm.Xp],outputs=[self.log_likelihood()])
+
+	def grad(self):
+		grad = T.grad(self.log_likelihood(),self.params)
+		fn = function(inputs=[self.kernel.X, self.kernel.Xp,self.Kmm.X,self.Kmm.Xp,self.Kmn.X,self.Kmn.Xp,self.Knm.X,self.Knm.Xp], outputs=grad)
+		return fn
+
+	def plain_gradient_descent(self,maxiters =1000, step=0.01):
+		gf = self.grad()
+		L = self.getL()
+		costs = []
+		iters = 0
+		prevcost = np.infty
+		currcost = L(self.xtrain,self.xtrain,self.pseudo_points.eval(),self.pseudo_points.eval(),
+			self.pseudo_points.eval(),self.xtrain,self.xtrain,self.pseudo_points.eval())[0]
+		costs.append(currcost)
+		while iters<maxiters:
+		    updates = gf(self.xtrain,self.xtrain)
+		    costs.append(L(self.xtrain,self.xtrain)[0])
+		    for update,param in zip(updates,self.params):
+		    	param.set_value(np.float64(param.get_value()-step * np.clip(update,-0.1,0.1)))
+		    iters+=1
+		    prevcost = costs[iters-1]
+		    currcost = costs[iters]
+		return costs
--- kernels.py
+++ kernels.py
@@ -1,18 +1,29 @@
 from __future__ import division
 from theano import tensor as T
 from theano import function
+from theano import shared
+import numpy as np
 
+"""
+ARD kernel, hyperparameters:c, b_1 .. b_D 
+"""
 class ARDKernel(object):
-    def __init__(self):
-        self.theta = T.fscalar("theta")
-        self.nu = T.fvector("nu")
-        self.X = T.fmatrix('X')
-        self.Y = T.fmatrix('Y')
-        scaledX = T.sqrt(self.nu)*self.X
-        scaledY = self.Y;
-        squared_euclidean_distances = ((scaledX.reshape((scaledX.shape[0], 1, -1)) - scaledY.reshape((1, scaledY.shape[0], -1)))**2).sum(2)
+    def __init__(self,D,b = None,c = None):
+    	if(b==None):
+    		self.b = shared(np.ones(D).astype(np.float64),"b")
+    	else:
+    		self.b = b
+    	if(c==None):
+        	self.c = shared(np.float64(1.),"c")
+        else:
+        	c = c
+        self.X = T.dmatrix('X')
+        self.Xp = T.dmatrix('Xp')
+        scaledX = self.b*self.X
+        scaledXp = self.b*self.Xp
+        squared_euclidean_distances = (scaledX ** 2).sum(1).reshape((scaledX.shape[0], 1)) + (scaledXp ** 2).sum(1).reshape((1, scaledXp.shape[0])) - 2 * scaledX.dot(scaledXp.T)
         xp = 0.5*squared_euclidean_distances
-        self.exp = self.theta*T.exp(-xp)
-        self.C = function(inputs=[self.X, self.Y,self.nu,self.theta], outputs=[self.exp])
-    def evaluate(self,X,Y,nu,theta):
-        return self.C(X,Y,nu,theta)[0]
+        self.exp = self.c*T.exp(-xp)
+        self.C = function(inputs=[self.X, self.Xp], outputs=[self.exp])
+    def evaluate(self,X,Y):
+        return self.C(X,Y)[0]
