9bcd2f2e523ef94a248a3b6a4bab306208052f57
Polina Vasiljeva <polinava@kth.se>
2016-01-13 17:57:48 +0100

    edited gradient.py to only include SPGP implementation, edited params()

--- gradient.py
+++ gradient.py
@@ -1,5 +1,6 @@
 import numpy as np
 from utils import *
+from sys import argv
 
 trainX, trainY, testX, testY = get_all_data("kin40k")
 
@@ -10,26 +11,17 @@ print trainY.shape
 print testX.shape
 print testY.shape
 
+# define the number of pseudo-input
+M = 10
+# define D
+D = trainX.shape[1]
+# define N
+N = trainX.shape[0]
 
-def kernel():
-	return 0
-
-
-def loglikelihood(M=None):
-	return 0
-
-
-def gamma_prime():
-	return 0
-
-def gradsum():
-	fi1_p = 
-	return 0
-
-def params(x0, SPGP=False):
+def params(x0):
 	# extracts the params and stores them into individual variables
 	# params: 
-	# x0 = array with initial values of the parameters
+	# x0 = an np.array with initial values of the parameters
 	# 
 	# extract sigma^2
 	sigma = x0[0]
@@ -37,19 +29,21 @@ def params(x0, SPGP=False):
 	c = x0[1]
 	# extract b
 	b = x0[2:D+2]
-	if SPGP==True:
-		# extract pseudo-inputs
-		X = x0[D+3:]
-		return sigma, c, b, X
-	return sigma, c, b
+	# extract pseudo-inputs
+	X = x0[D+3:].reshape([M,D])
+	return sigma, c, b, X
 
-def gradient(SPGP=False, M=None):
-	#if SPGP == True:
-		# gradient_vec.append()
-		# compute gradient wrt pseudo-inputs
+def loglikelihood():
+	return 0
+
+def gradient(x0):
+	sigma, c, b, X = params(x0)
+	# gradient_vec.append()
+	# compute gradient wrt pseudo-inputs
+
+	sigma, c, b, X = params(x0)
 
 	# gradient wrt b
-
 
 	# gradient wrt c
 
